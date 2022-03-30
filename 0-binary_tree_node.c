f4b2620d1a1b5eb4e5afc86b0d1fd02e66f0ec63
sammyALX <samuelhailu49@gmail.com>
2022-03-30 16:53:09 +0300

    simple change to add redeablity

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,8 +11,9 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
+	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 	{
 		return (NULL);
