8cf0ec8f453a49be0294fce054e83312a6a6d2ef
sammyALX <samuelhailu49@gmail.com>
2022-03-31 11:31:02 +0300

    simplify binary_tree_delete implementation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = NULL;
 
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,67 +10,10 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *current;
-
-	if (tree == NULL)
+	if (tree != NULL)
 	{
-		return;
-	}
-	current = subtree_left(tree);
-	while (current->parent)
-	{
-		current = current->parent;
-		if (current->left)
-		{
-			free(current->left);
-			current->left = NULL;
-		}
-		if (current->right)
-		{
-			current = subtree_left(current->right);
-			current = current->parent;
-			free(current->right);
-			current->right = NULL;
-		}
-	}
-	free(current);
-}
-
-/**
- * subtree_left - iterate on the left subtree of a tree
- * @subtree: pointer to the root of the subtree
- *
- * Return: the most left node of the subtree if there is no
- * the most right node of the subtree if there is no
- * the root node of the subtree
- */
-
-binary_tree_t *subtree_left(binary_tree_t *subtree)
-{
-	binary_tree_t *current;
-
-	if (subtree == NULL)
-	{
-		return (subtree);
-	}
-	else
-	{
-		current = subtree;
-		while (current)
-		{
-			if (current->left)
-			{
-				current = current->left;
-			}
-			else if (current->right)
-			{
-				current = current->right;
-			}
-			else
-			{
-				break;
-			}
-		}
-		return (current);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
 	}
 }
