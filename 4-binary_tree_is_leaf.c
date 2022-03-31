b03fe345a71b2e25363ae81736cd9f65d64eb236
SammyAlx <samuelhailu49@gmail.com>
2022-03-31 22:09:36 +0300

    add type for the node argument

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  * Return: 1 if it is node and 0 if not or node is NULL
  */
 
-int binary_tree_is_leaf(binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
