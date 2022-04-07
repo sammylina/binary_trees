1af3a97a80d2e47d2cd4cba1ef0863c0c9966370
SammyAlx <samuelhailu49@gmail.com>
2022-04-07 06:01:38 +0300

    reimplement balance factor calulator

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,12 +19,48 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree->left)
 	{
-		left_height = binary_tree_height(tree->left);
+		left_height++;
+		left_height += binary_tree_height(tree->left);
 	}
 	if (tree->right)
 	{
-		right_height = binary_tree_height(tree->right);
+		right_height++;
+		right_height += binary_tree_height(tree->right);
 	}
-	printf("left: %d, right: %d \n", left_height, right_height);
 	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - find height of a tree
+ * @tree: pointer to the root of a subtree
+ *
+ * Return: height of subtree or 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_subtree_height = 0;
+	size_t left_subtree_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	while (tree)
+	{
+		if (tree->left)
+		{
+			left_subtree_height++;
+			left_subtree_height += binary_tree_height(tree->left);
+		}
+		if (tree->right)
+		{
+			right_subtree_height++;
+			right_subtree_height += binary_tree_height(tree->right);
+		}
+		break;
+	}
+	if (right_subtree_height > left_subtree_height)
+		return (right_subtree_height);
+	else
+		return (left_subtree_height);
+
 }
