7b2028475c0cb8265a480624689c355b3e23610d
sammyALX <samuelhailu49@gmail.com>
2023-01-28 13:38:01 +0300

    set right node of the new node to NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -26,7 +26,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 
 	right_node->n = value;
-	right_node->right = NULL;
+	right_node->left = NULL;
 
 	if (parent->right)
 	{
